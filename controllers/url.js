29923df506cd4c69f10d435c4fbe3ab0e0491e72
viveksighh930 <tomarvivek669@gmail.com>
2024-12-24 21:44:22 +0530

    analytics GET

--- controllers/url.js
+++ controllers/url.js
@@ -1,4 +1,4 @@
-const shortid= require("shortid")
+const shortid = require("shortid")
 const URL = require("../models/url")
 async function handleGenerateNewShortURL(req, res) {
     const body = req.body;
@@ -8,10 +8,19 @@ async function handleGenerateNewShortURL(req, res) {
     await URL.create({
         shortId: shortId,
         redirectURL: body.url,
-        visitHistory:[],
+        visitHistory: [],
     })
-    return res.json({id:shortId})
+    return res.json({ id: shortId })
 }
-module.exports= ({
+async function handleGetAnalytics(req, res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId })
+    return res.json({
+         totalClicks: result.visitHistory.length,
+         analytics:result.visitHistory, })
+
+}
+module.exports = ({
     handleGenerateNewShortURL,
+    handleGetAnalytics,
 })
